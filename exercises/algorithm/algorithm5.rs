28e9044d996eee2d08ce279ce3d15882de22d966
梁益 <liang-yi-666@foxmail.com>
2024-04-24 21:19:58 +0800

    update

--- exercises/algorithm/algorithm5.rs
+++ exercises/algorithm/algorithm5.rs
@@ -3,7 +3,6 @@
 	This problem requires you to implement a basic BFS algorithm
 */
 
-//I AM NOT DONE
 use std::collections::VecDeque;
 
 // Define a graph
@@ -31,6 +30,20 @@ impl Graph {
 		//TODO
 
         let mut visit_order = vec![];
+        let mut visited = vec![false; self.adj.len()];
+        let mut queue = VecDeque::new();
+        visited[start] = true;
+        queue.push_back(start);
+        while let Some(node) = queue.pop_front() {
+            visit_order.push(node);
+            for &ne in &self.adj[node]{
+                if visited[ne]  == false{
+                    visited[ne] = true;
+                    queue.push_back(ne);
+                }
+            }
+        }
+
         visit_order
     }
 }
